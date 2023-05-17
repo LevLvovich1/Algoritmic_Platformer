b15d6717becdacef23814e1125824b0e6b8776a9
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-17 18:27:02 +0300

    Update Teleport.cs

--- Teleport.cs
+++ Teleport.cs
@@ -1,1 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Missile : MonoBehaviour
+{
+    public Transform teleportPoint;
+    void OnTriggerEnter
+    {
+    
+    }
+}
