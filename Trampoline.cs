221a014867f8c0ab333e9078e085d602da62b4ae
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-25 20:33:35 +0300

    Скрипт закончен.

--- Trampoline.cs
+++ Trampoline.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Trampoline : MonoBehaviour
 {
-   
     void OnTriggerEnter(Collider other)
     {
-      other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+        //Увеличение силы прыжка
+        other.GetComponent<Jump>().jumpStrength = 10;
+ 
     }
-
+ 
     void OnTriggerExit(Collider other)
     {
-      
+        //Уменьшение силы прыжка
+        other.GetComponent<Jump>().jumpStrength = 2;
+ 
     }
-    
 }
