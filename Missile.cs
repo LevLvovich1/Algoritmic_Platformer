4ce460c92f67de18d7c45ac7f0ddc65a44743fe9
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-25 20:14:25 +0300

    Скрипт закончен.

--- Missile.cs
+++ Missile.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Missile : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //Скорость полёта огненного шара
+    public float speed;
+ 
     void Update()
     {
-        
+       //Клон огненного шара уничтожается через 3 секунды
+        Destroy(gameObject, 3);
+
+          //Каждый кадр позиция шара обновляется на произведение вектора движения вперёд (0,0,1),
+        //скорости движения шара и значение разницы в секундах между последним и текущим кадрами
+
+        transform.position += transform.forward * speed * Time.deltaTime;
     }
+ 
+    void OnTriggerEnter(Collider other) {
+ 
+        //Враг уничтожается
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+ 
+        //Снаряд уничтожается
+        Destroy(gameObject);
+ 
+    }
+ 
 }
