ae4df38405c8e765dbed8f2d4f3f54144ad861be
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-25 20:33:02 +0300

    Скрипт закончен.

--- Teleport.cs
+++ Teleport.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Missile : MonoBehaviour
+ 
+public class Teleport : MonoBehaviour
 {
     public Transform teleportPoint;
+ 
     void OnTriggerEnter(Collider other)
     {
-        
+        //Меняет позицию игрока на позицию другого телепорта
+        other.transform.position = teleportPoint.position;
     }
 }
