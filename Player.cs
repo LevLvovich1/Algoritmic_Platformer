931a1e5625283a873ff63f95643ef128dbb37def
LevLvovich1 <131392712+LevLvovich1@users.noreply.github.com>
2023-05-25 20:15:14 +0300

    Скрипт закончен.

--- Player.cs
+++ Player.cs
@@ -1,25 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+ 
 public class Player : MonoBehaviour
 {
-    public GameObject fireballPrefab;
-    public int coins;
-    public Transform attackPoint;
+    //Текущее здоровье игрока
     private int health = 10;
-    
-    // Update is called once per frame
+ 
+    //Число собранных монет
+    private int coins;
+ 
+    //Префаб огненного шара и параметр Transform точки атаки
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+ 
+ 
+   //Компонент, отвечающий за проигрывание звуков
+    public AudioSource audioSource;
+ 
+    //Звуковой файл, содержащий звуковой эффект нанесения урона
+    public AudioClip damageSound;
+ 
+    //Метод, обрабатывающий нанесённый урон
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+ 
+        //Если здоровье ещё есть, то проигрывается звук нанесения урона
+        if(health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            //print("Здоровье игрока: " + health);
+        }
+        //Если здоровья нет, то перезапускается текущая сцена
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+ 
+    //Метод, увеличивающий число монеток
+    public void CollectCoins()
+    {
+        coins++;
+        print("Собранные монетки: " + coins);
+    }
+ 
+ 
     void Update()
     {
+ 
+        //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
         }
-    }
-    
-    public void CollectCoins()
-    {
         
     }
 }
